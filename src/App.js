f40f118be9e030f9742b15f0ff9a97c841af9cc4
Mazen El Ali <mazenelali>
2022-12-19 10:30:54 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
-
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg"
 
 import "./App.css";
 
@@ -21,9 +18,56 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+        <header>
+        <form >
+          <input  type="text" class="input"></input>
+          <input type={"submit"} value={"Find weather"} class="submit"></input>
+        </form>
+        </header>
+          <div className="content">
+            <div class="sun">
+              <img src={mostlycloudy} alt="cloudy" />
+              <p>overcast cloud</p>
+            </div>
+            <div className="caption">
+            
+            <p><b>temperature</b> <deg>10</deg> to <deg>11</deg> </p>
+            <p><span class="humidity"><b>Humidity</b> 78% <b>Pressure</b>  1008.48</span></p>
+            </div>
+            
+            <div class="all-days">
+            <div class="firstDay">
+            <time>3:00</time>
+            <img src={mostlycloudy} alt="clear"></img>
+            <p>8C</p>
+            </div>
+            <div class="firstDay">
+            <time>6:00</time>
+            <img src={mostlycloudy} alt="clear"></img>
+            <p>14C</p>
+            </div>
+            <div class="firstDay">
+            <time>9:00</time>
+            <img src="clear.svg" alt="clear"></img>
+            <p>17C</p>
+            </div>
+            <div class="firstDay">
+            <time>12:00</time>
+            <img src="clear.svg" alt="clear"></img>
+            <p>18C</p>
+            </div>
+            <div class="firstDay">
+            <time>15:00</time>
+            <img src="clear.svg" alt="clear"></img>
+            <p>16C</p>
+            </div>
+            <div class="firstDay">
+            <time>18:00</time>
+            <img src={mostlycloudy} alt="clear"></img>
+            <p>13C</p>
+          </div>
+          </div>
+        </div>
       </div>
     );
   }
